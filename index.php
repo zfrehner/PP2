2a3ed9a3b1869c7c2f0888fc6c03977f695c01c6
Zach Frehner <zfrehner@mail.greenriver.edu>
2020-04-17 16:20:46 -0400

    added index file

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//Zach Frehner
+//pair program 2
+//4/17/2020
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+<?php
+echo "<h2>PHP Array Practice</h2>";
+?>
+
+</body>
+</html>
+
